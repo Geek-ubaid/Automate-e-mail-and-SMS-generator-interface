511df5bb3f016c0d8f2a3af45ebf99f8e1dca3f9
Geek-ubaid <31818185+Geek-ubaid@users.noreply.github.com>
2018-02-08 20:28:56 +0530

    mail interface

--- main.py
+++ main.py
@@ -2,47 +2,118 @@
 
 # Form implementation generated from reading ui file 'main.ui'
 #
-# Created by: PyQt5 UI code generator 5.6
+# Created by: PyQt5 UI code generator 5.10
 #
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+import re
 
 class Ui_MainWindow(object):
+    def fileextract(self):
+        rec = self.recipients.text()
+        if (str(rec)[len(rec)-4::]==".txt"):
+
+    def fileextractm(self):
+        recm=self.recipientsm.text()
+        if (str(rec)[len(rec)-4::]==".txt"):
+            file=open(recm,"r")
+            new=open("mail.txt","a+")
+            for i in file.readlines():
+                data=re.findall(r'[\w\.-]+@[\w\.-]+', i)
+
+
+
+
+
+
+    def sendemail(self):
+        msg=self.message.text()
+
+    def sendmessage(self):
+        msgm=self.messaget.text()
+
+
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(893, 654)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.tabWidget = QtWidgets.QTabWidget(self.centralwidget)
-        self.tabWidget.setGeometry(QtCore.QRect(10, 20, 861, 621))
+        self.tabWidget.setGeometry(QtCore.QRect(10, 10, 861, 621))
+        font = QtGui.QFont()
+        font.setPointSize(8)
+        self.tabWidget.setFont(font)
         self.tabWidget.setObjectName("tabWidget")
         self.tab = QtWidgets.QWidget()
         self.tab.setObjectName("tab")
-        self.lineEdit_2 = QtWidgets.QLineEdit(self.tab)
-        self.lineEdit_2.setGeometry(QtCore.QRect(190, 90, 471, 31))
-        self.lineEdit_2.setObjectName("lineEdit_2")
-        self.lineEdit = QtWidgets.QLineEdit(self.tab)
-        self.lineEdit.setGeometry(QtCore.QRect(190, 20, 471, 31))
-        self.lineEdit.setObjectName("lineEdit")
-        self.textBrowser = QtWidgets.QTextBrowser(self.tab)
-        self.textBrowser.setGeometry(QtCore.QRect(50, 240, 711, 281))
-        self.textBrowser.setObjectName("textBrowser")
+        self.subject = QtWidgets.QLineEdit(self.tab)
+        self.subject.setGeometry(QtCore.QRect(190, 120, 471, 31))
+        self.subject.setObjectName("subject")
+        sub=self.subject.text()
+        self.recipients = QtWidgets.QLineEdit(self.tab)
+        self.recipients.setGeometry(QtCore.QRect(190, 30, 471, 31))
+        self.recipients.setObjectName("rercipients")
+
         self.label_2 = QtWidgets.QLabel(self.tab)
-        self.label_2.setGeometry(QtCore.QRect(50, 89, 81, 31))
+        self.label_2.setGeometry(QtCore.QRect(50, 120, 81, 31))
         self.label_2.setObjectName("label_2")
         self.label = QtWidgets.QLabel(self.tab)
-        self.label.setGeometry(QtCore.QRect(50, 20, 71, 31))
+        self.label.setGeometry(QtCore.QRect(50, 30, 71, 31))
         self.label.setObjectName("label")
         self.label_3 = QtWidgets.QLabel(self.tab)
-        self.label_3.setGeometry(QtCore.QRect(50, 180, 81, 31))
+        self.label_3.setGeometry(QtCore.QRect(50, 200, 81, 31))
         self.label_3.setObjectName("label_3")
-        self.pushButton = QtWidgets.QPushButton(self.tab)
-        self.pushButton.setGeometry(QtCore.QRect(200, 140, 75, 23))
-        self.pushButton.setObjectName("pushButton")
+        self.attachment = QtWidgets.QPushButton(self.tab)
+        self.attachment.setGeometry(QtCore.QRect(200, 170, 75, 23))
+        self.attachment.setObjectName("attachment")
+        self.send = QtWidgets.QPushButton(self.tab)
+        self.send.setGeometry(QtCore.QRect(680, 550, 75, 23))
+        self.send.setObjectName("send")
+        self.send.clicked.connect(sendemail)
+        self.extract = QtWidgets.QPushButton(self.tab)
+        self.extract.setGeometry(QtCore.QRect(690, 30, 75, 23))
+        self.extract.setObjectName("extract")
+        self.extract.clicked.connect(self.fileextract)
+        self.message = QtWidgets.QPlainTextEdit(self.tab)
+        self.message.setGeometry(QtCore.QRect(70, 270, 681, 251))
+        self.message.setObjectName("message")
         self.tabWidget.addTab(self.tab, "")
         self.tab_2 = QtWidgets.QWidget()
         self.tab_2.setObjectName("tab_2")
+        self.label_4 = QtWidgets.QLabel(self.tab_2)
+        self.label_4.setGeometry(QtCore.QRect(90, 180, 121, 51))
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(14)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label_4.setFont(font)
+        self.label_4.setObjectName("label_4")
+        self.messaget = QtWidgets.QPlainTextEdit(self.tab_2)
+        self.messaget.setGeometry(QtCore.QRect(80, 250, 571, 251))
+        self.messaget.setObjectName("messaget")
+        self.sendm = QtWidgets.QPushButton(self.tab_2)
+        self.sendm.setGeometry(QtCore.QRect(700, 280, 75, 23))
+        self.sendm.setObjectName("sendm")
+        self.sendm.clicked.connect(sendmessage)
+        self.label_5 = QtWidgets.QLabel(self.tab_2)
+        self.label_5.setGeometry(QtCore.QRect(100, 70, 111, 16))
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(13)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label_5.setFont(font)
+        self.label_5.setObjectName("label_5")
+        self.recipientsm = QtWidgets.QLineEdit(self.tab_2)
+        self.recipientsm.setGeometry(QtCore.QRect(290, 60, 231, 31))
+        self.recipientsm.setObjectName("recipientsm")
+        recp=self.recipientsm.text()
+        self.extractm = QtWidgets.QPushButton(self.tab_2)
+        self.extractm.setGeometry(QtCore.QRect(600, 70, 75, 23))
+        self.extractm.setObjectName("extractm")
+        self.extractm.clicked.connect(fileextractm)
         self.tabWidget.addTab(self.tab_2, "")
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
@@ -50,23 +121,23 @@ class Ui_MainWindow(object):
         MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
-        self.tabWidget.setCurrentIndex(0)
+        self.tabWidget.setCurrentIndex(1)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.textBrowser.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
-"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>"))
         self.label_2.setText(_translate("MainWindow", "Subject"))
-        self.label.setText(_translate("MainWindow", "To"))
+        self.label.setText(_translate("MainWindow", "Recipients:"))
         self.label_3.setText(_translate("MainWindow", "Message"))
-        self.pushButton.setText(_translate("MainWindow", "Attachment"))
+        self.attachment.setText(_translate("MainWindow", "Attachment"))
+        self.send.setText(_translate("MainWindow", "SEND "))
+        self.extract.setText(_translate("MainWindow", "Extract"))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), _translate("MainWindow", "E-mail"))
+        self.label_4.setText(_translate("MainWindow", "Your message"))
+        self.sendm.setText(_translate("MainWindow", "SEND"))
+        self.label_5.setText(_translate("MainWindow", "Recipients"))
+        self.extractm.setText(_translate("MainWindow", "Extract"))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), _translate("MainWindow", "SMS"))
 
 
